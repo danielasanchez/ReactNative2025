d8e845ae1f9175a99751e6275d96f1bb03858251
Daniela Sanchez <daniela.sanchez@tectijuana.edu.mx>
2025-11-23 01:09:54 -0800

    Componentes en una pantalla

---
+++ example3/Screens/HomeScreen/Header.js
@@ -0,0 +1,20 @@
+import { StyleSheet, Text, View } from 'react-native'
+
+const Header = () => {
+    return (
+        <View style={styles.container}>
+            <Text>Header</Text>
+        </View>
+    )
+}
+
+export default Header
+
+const styles = StyleSheet.create({
+    container:{
+        backgroundColor:'#196e2b',
+        alignItems:'center',
+        justifyContent:'center',
+        flex:1//height:'25%',
+    },   
+})
---
+++ example3/Screens/HomeScreen/index.js
@@ -0,0 +1,14 @@
+import { View } from 'react-native'
+import Body from './Body'
+import Footer from './Footer'
+import Header from './Header'
+
+export default function Home() {
+    return (
+        <View style={{flex:1}}>
+            <Header/>
+            <Body/>
+            <Footer/>
+        </View>
+    )
+}
